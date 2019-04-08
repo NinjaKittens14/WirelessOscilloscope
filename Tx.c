f4da1530c0d9eaff430e55c5f4fa75c3dbd6b7d2
Peter <amundsenpeter@gmail.com>
2019-04-08 11:03:31 -0400

    Some more setup

--- Tx.c
+++ Tx.c
@@ -3,12 +3,12 @@
 #include "stdint.h"
 #include "wireless-v2.h"
 
-/**
- * Tx.c
- * Receive Analog signal and input into ADC to convert to Digital data.
- * Send Digital data to wireless card and transmit to Receiver card.
- * 4/8/19
- */
+//------------------------------------------------------------------------------
+// Tx.c
+// Receive Analog signal and input into ADC to convert to Digital data.
+// Send Digital data to wireless card and transmit to Receiver card.
+// 4/8/19
+//------------------------------------------------------------------------------
 
 #pragma vector=ADC10_VECTOR
 __interupt void IsrAdc10LedSwitch(void)
@@ -26,28 +26,63 @@ __interupt void IsrAdc10LedSwitch(void)
 
 
 
-void main ()
-/**
- * Func: Initialize ADC10 and cc2250 wrireless card
- * Args: None
- * Retn: None
- */
+void Setup()
+//-----------------------------------------------------------------------------
+// Func:  Setup MSP430 Ports & Clocks, and reset & config cc2500 chip
+// Args:  none
+// Retn:  none
+//------------------------------------------------------------------------------
 {
-    WDTCTL  = WDTPW + WDTHOLD;  // Stop Watchdog (Yawn);
+    volatile uint16_t delay;
 
+    for(delay=0; delay<650; delay++){};   // Empirical: cc2500 Pwr up settle
+
+    //Crystal Setup
     DCOCTL  = CALDCO_8MHZ;      // DCO = 8 MHz;
     BCSCTL1 = CALBC1_8MHZ;      // DCO = 8 MHz;
-    P3DIR  |=  0x02;            // P3.1 = output (to external LED);
-    P3OUT  &= ~0x02;            // Init P3.1 = 0 (LED off)
+    BCSCTL2 |= DIVS_3;          // SMCLK = MCLK/8 = 1MHz
 
+    // ADC Setup
     ADC10AE0  |= 0x01;          // Enable chnl A0 = P2.0;
     ADC10CTL0 |= ADC10ON        // Turn ON ADC10
-              |  ADC10IE        // enable ADC IRQ
-              |  ADC10SHT_2;    // samp-hold tim = 16 cyc;
+                 |  ADC10IE        // enable ADC IRQ
+                 |  ADC10SHT_2;    // samp-hold tim = 16 cyc;
     ADC10CTL1 |= ADC10SSEL_3    // ADC10CLK source = SMCLK
-              |  ADC10DIV_3     // ADC10CLK divider = 4
-              |  INCH_0;        // Select input = chnl A0 (default);
+                 |  ADC10DIV_3     // ADC10CLK divider = 4
+                 |  INCH_0;        // Select input = chnl A0 (default);
 
+    // Timer Config
+    TACTL   = TASSEL_2 |ID_3 | MC_1;      // TA uses SMCLK/8, in Up mode
+    TACCR0  = 60000;                      // ~480 msec @  1/8 MHz
+    TACCTL0 = CCIE;		                // enable TA CCR0 IRQ
+
+    // LED Port config
+    P1DIR |=  0x03;                       // Set LED pins to output
+    P1OUT &= ~0x03;                       // Clear LEDs
+
+    // Wireless Initialization
+    P2SEL = 0;                            // P2.6, P2.7 = GDO0, GDO2 (GPIO)
+    TI_CC_SPISetup();                     // Init SPI port for cc2500
+    TI_CC_PowerupResetCCxxxx();           // Reset cc2500
+    writeRFSettings();                    // Put settings to cc2500 config regs
+
+    TI_CC_SPIWriteReg(TI_CCxxx0_CHANNR,  0);  // Set Your Own Channel Number
+                                              // only AFTER writeRFSettings
+
+    for(delay=0; delay<650; delay++){};   // Empirical: Let cc2500 finish setup
+
+    P1OUT = 0x02;                         // Setup done => Turn on green LED
+}
+
+void main ()
+//----------------------------------------------------------------------------
+// Func: Initialize ADC10 and cc2250 wrireless card
+// Args: None
+// Retn: None
+//----------------------------------------------------------------------------
+{
+    WDTCTL  = WDTPW + WDTHOLD;  // Stop Watchdog (Yawn);
+    Setup();                    // initialize ADC and cc2250
     while(1) {
         ADC10CTL0 |= ENC | ADC10SC;           // Enable Conversion
         __bis_SR_register(LPM0_bits + GIE);   // Sleep until interupt from ADC

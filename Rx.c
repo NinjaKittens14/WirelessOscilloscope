f2f79fe474eec551df517ed5c090c0ece254069d
Peter <amundsenpeter@gmail.com>
2019-04-22 10:11:33 -0400

    fixed indentation on Rx

--- Rx.c
+++ Rx.c
@@ -23,40 +23,40 @@ union {
 #pragma vector=PORT2_VECTOR        // cc2500 pin GDO0 is hardwired to P2.6
 __interrupt void PktRxedISR(void)
 {
-  // Buffer Len for rxPkt = only data bytes;  
-  // pkt size byte not incl b/c it is stripped off by RFReceivePacket func.
-  // IF address check was enabled, THEN would need 1 more byte for device addr
+    // Buffer Len for rxPkt = only data bytes;
+    // pkt size byte not incl b/c it is stripped off by RFReceivePacket func.
+    // IF address check was enabled, THEN would need 1 more byte for device addr
 
-  static uint8_t len = 2;          // Packet Len = 3 bytes (data only)
-  uint8_t status[2];               // Buffer to store pkt status bytes    
-  uint8_t rxPkt[2];
-  
-  if(TI_CC_GDO0_PxIFG & TI_CC_GDO0_PIN)            // chk GDO0 bit of P2 IFG Reg
-  { 
-	P1OUT ^= 0x01;								   // Toggle LED
-    RFReceivePacket(rxPkt, &len, status);  		   // Get packet from cc2500
-  }
-  TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;             // Reset GDO0 IRQ flag
- 
-  TI_CC_SPIStrobe(TI_CCxxx0_SIDLE);      // Set cc2500 to idle mode.
-  TI_CC_SPIStrobe(TI_CCxxx0_SRX);        // Set cc2500 to RX mode.
-                                         // AutoCal @ IDLE to RX Transition
+    static uint8_t len = 2;          // Packet Len = 3 bytes (data only)
+    uint8_t status[2];               // Buffer to store pkt status bytes
+    uint8_t rxPkt[len];
 
-  // Parse rxPkt into adcValue
-  adcValue.u8[1] = rxPkt[1];			  // MSb
-  adcValue.u8[0] = rxPkt[0];			  // LSb
-  
-  // Wait for SPI Bus
-  while(UCB0STAT & UCBUSY){};       // Wait for SPI Bus to be clear
-  P4OUT &= ~0x20;                   // Assert SS (active low)
+    if(TI_CC_GDO0_PxIFG & TI_CC_GDO0_PIN)            // chk GDO0 bit of P2 IFG Reg
+    {
+        P1OUT ^= 0x01;								   // Toggle LED
+        RFReceivePacket(rxPkt, &len, status);  		   // Get packet from cc2500
+    }
+    TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;             // Reset GDO0 IRQ flag
 
-  UCB0TXBUF = adcValue.u8[1];    // send byte
-  while(!(IFG2 & UCB0TXIFG)){};  // wait for TxBUF to be empty
-  UCB0TXBUF = adcValue.u8[0];    // send byte
-  while(!(IFG2 & UCB0TXIFG)){};  // wait for TxBuf to be empty
-  
-  while (UCB0STAT & UCBUSY){};     // wait for quiet bus
-  P4OUT |= 0x20;                   // de-assert SS (active low)
+    TI_CC_SPIStrobe(TI_CCxxx0_SIDLE);      // Set cc2500 to idle mode.
+    TI_CC_SPIStrobe(TI_CCxxx0_SRX);        // Set cc2500 to RX mode.
+                                           // AutoCal @ IDLE to RX Transition
+
+    // Parse rxPkt into adcValue
+    adcValue.u8[1] = rxPkt[1];			  // MSb
+    adcValue.u8[0] = rxPkt[0];			  // LSb
+
+    // Wait for SPI Bus
+    while(UCB0STAT & UCBUSY){};       // Wait for SPI Bus to be clear
+    P4OUT &= ~0x20;                   // Assert SS (active low)
+
+    UCB0TXBUF = adcValue.u8[1];    // send byte
+    while(!(IFG2 & UCB0TXIFG)){};  // wait for TxBUF to be empty
+    UCB0TXBUF = adcValue.u8[0];    // send byte
+    while(!(IFG2 & UCB0TXIFG)){};  // wait for TxBuf to be empty
+
+    while (UCB0STAT & UCBUSY){};     // wait for quiet bus
+    P4OUT |= 0x20;                   // de-assert SS (active low)
 }
 
 //------------------------------------------------------------------------------
@@ -66,62 +66,62 @@ __interrupt void PktRxedISR(void)
 //------------------------------------------------------------------------------
 void SetupAll(void)
 {   
-  volatile uint16_t delay;
+    volatile uint16_t delay;
 
-  for(delay=0; delay<650; delay++);     // Empirical: cc2500 Pwr up settle
-   
-  // set up clock system
-  BCSCTL1 = CALBC1_8MHZ;                // set DCO = 8MHz
-  DCOCTL  = CALDCO_8MHZ;                // set DCO = 8MHz
-  BCSCTL2 |= DIVS_0;                    // SMCLK = MCLK/8 = 1MHz
-  
-  // Config built-in LED
-  P1DIR |= 0x01;						// P1.0 = output
-  P1OUT &= ~0x01;						// Set LED as Off
-  
-  // Config SMCLK Output to pin P2.1
-  P2SEL |= 0x02;						// P2.1 alt mode (SMCLK Output)
-  P2DIR |= 0x02;						// P2.1 Output dir
+    for(delay=0; delay<650; delay++);     // Empirical: cc2500 Pwr up settle
 
-  // Config. USCA0 for SPI Master
-  P3SEL = 0x10;                         // P3.4 alt modes (MOSI & SCLK)
-  P3DIR = 0x10;                         // P3.4 output dir
-  // Leave P3.0 alone for UCB RF SPI SS*, use SMCLK output pin P2.1 for UCLK
+    // set up clock system
+    BCSCTL1 = CALBC1_8MHZ;                // set DCO = 8MHz
+    DCOCTL  = CALDCO_8MHZ;                // set DCO = 8MHz
+    BCSCTL2 |= DIVS_0;                    // SMCLK = MCLK/8 = 1MHz
 
-  P4SEL &= ~0x20;                       // P4.5 GPIO (SS)
-  P4DIR |= 0x20;                        // P4.5 Output dir
-  P4OUT |= 0x20;                        // clear SS (active low)
+    // Config built-in LED
+    P1DIR |= 0x01;						// P1.0 = output
+    P1OUT &= ~0x01;						// Set LED as Off
 
-  UCA0CTL0 |= UCSYNC | UCMST | UCMSB;   // synchronous, 3-pin Master, MSB first
-  UCA0CTL1 |= UCSSEL_2;        			// SCLK source = SMCLK (8sMHz)
-  UCA0CTL1 &= ~UCSWRST;
-  
-  // Wireless Initialization
-  P2SEL = 0;                            // P2.6, P2.7 = GDO0, GDO2 (GPIO)
-  TI_CC_SPISetup();                     // Initialize SPI port
-  TI_CC_PowerupResetCCxxxx();           // Reset cc2500
-  writeRFSettings();                    // Send RF settings to config regs
-  UCB0BR0 = 0x02;                       // UCLK/2 = SMCLK/2 = 4MHz
+    // Config SMCLK Output to pin P2.1
+    P2SEL |= 0x02;						// P2.1 alt mode (SMCLK Output)
+    P2DIR |= 0x02;						// P2.1 Output dir
 
-  TI_CC_GDO0_PxIES |=  TI_CC_GDO0_PIN;  // IRQ on GDO0 fall. edge (end of pkt)
-  TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;  // Clear  GDO0 IRQ flag
-  TI_CC_GDO0_PxIE  |=  TI_CC_GDO0_PIN;  // Enable GDO0 IRQ 
-  TI_CC_SPIStrobe(TI_CCxxx0_SRX);       // Init. cc2500 in RX mode.
-  
-  TI_CC_SPIWriteReg(TI_CCxxx0_CHANNR, CHANNEL);    // Set Your Own Channel Number
-                                             // AFTER writeRFSettings (???)
+    // Config. USCA0 for SPI Master
+    P3SEL = 0x10;                         // P3.4 alt modes (MOSI & SCLK)
+    P3DIR = 0x10;                         // P3.4 output dir
+    // Leave P3.0 alone for UCB RF SPI SS*, use SMCLK output pin P2.1 for UCLK
 
-  for(delay=0; delay<650; delay++);     // Empirical: Let cc2500 finish setup
+    P4SEL &= ~0x20;                       // P4.5 GPIO (SS)
+    P4DIR |= 0x20;                        // P4.5 Output dir
+    P4OUT |= 0x20;                        // clear SS (active low)
+
+    UCA0CTL0 |= UCSYNC | UCMST | UCMSB;   // synchronous, 3-pin Master, MSB first
+    UCA0CTL1 |= UCSSEL_2;        			// SCLK source = SMCLK (8sMHz)
+    UCA0CTL1 &= ~UCSWRST;
+
+    // Wireless Initialization
+    P2SEL = 0;                            // P2.6, P2.7 = GDO0, GDO2 (GPIO)
+    TI_CC_SPISetup();                     // Initialize SPI port
+    TI_CC_PowerupResetCCxxxx();           // Reset cc2500
+    writeRFSettings();                    // Send RF settings to config regs
+    UCB0BR0 = 0x02;                       // UCLK/2 = SMCLK/2 = 4MHz
+
+    TI_CC_GDO0_PxIES |=  TI_CC_GDO0_PIN;  // IRQ on GDO0 fall. edge (end of pkt)
+    TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;  // Clear  GDO0 IRQ flag
+    TI_CC_GDO0_PxIE  |=  TI_CC_GDO0_PIN;  // Enable GDO0 IRQ
+    TI_CC_SPIStrobe(TI_CCxxx0_SRX);       // Init. cc2500 in RX mode.
+
+    TI_CC_SPIWriteReg(TI_CCxxx0_CHANNR, CHANNEL);    // Set Your Own Channel Number
+                                               // AFTER writeRFSettings (???)
+
+    for(delay=0; delay<650; delay++);     // Empirical: Let cc2500 finish setup
 }
 
 // -----------------------------------------------------------------------------
 void main()
-{ WDTCTL = WDTPW + WDTHOLD;         // halt watchdog
+{   WDTCTL = WDTPW + WDTHOLD;         // halt watchdog
 
-  volatile uint8_t rxPkt[3];        // Buffer to store received pkt payload
-  SetupAll();                       // Setup clocks, ports & cc2500 settings
+    volatile uint8_t rxPkt[3];        // Buffer to store received pkt payload
+    SetupAll();                       // Setup clocks, ports & cc2500 settings
 
-  __bis_SR_register(CPUOFF + GIE);  // sleep til Pkt RX
+    __bis_SR_register(CPUOFF + GIE);  // sleep til Pkt RX
 }
 
 // ========================================================================= EOF
